086debc401eda35a39f326b71cfe82ce88f38be4
shreya789g <s3358372@gmail.com>
2026-09-01 11:02:32 +0530

    Github project create

---
+++ script.js
@@ -0,0 +1,90 @@
+const gitform=document.querySelector('#gitform');
+gitform.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    const gitusername=document.querySelector('#git-user-name').value 
+    console.log(gitusername);
+    fetchdata(gitusername)
+})
+async function fetchdata(username){
+    try{
+        const res=await fetch(`https://api.github.com/users/${username}`)
+        const user=await res.json()
+        console.log(user)
+        displaydata(user)
+    } catch(error){
+        console.log(error);
+        
+    }
+}
+function displaydata(userdata){
+    const userdetails=document.querySelector('#userdetail')
+    if(userdata.id){
+       userdetails.innerHTML=`<aside>
+      <img
+        src="https://avatars.githubusercontent.com/u/9292065?v=4"
+        alt="loading..."
+      />
+    </aside>
+    <article id="aboutme">
+      <h1>name:<span>${userdata.name}</span></h1>
+      <p>
+        Bio:<span>${userdata.bio}</span>
+      </p>
+      <p>Location:<span>${userdata.location}</span></p>
+      <div class="summary">
+        <button>repos:<span>${userdata.public_repos}</span></button>
+        <button>followers:<span>${userdata.followers}</span></button>
+        <button>following:<span>${userdata.following}</span></button>
+      </div>
+    </article>`
+    }
+    else{
+        userdetails.innerHTML=`<h1 style="color:orange; font-size:25px; padding:15px 50px;">Invalid User name</h1>`
+    }
+}
+
+
+
+// // this is my first step to get data(username) of user
+// const gitform = document.querySelector('#gitform');
+// gitform.addEventListener("submit", (e) => {
+//     e.preventDefault()
+//     const gitUserName = document.querySelector('#git-user-name').value
+//     console.log(gitUserName);
+//     fetchGitData(gitUserName)
+// })
+
+// // this is my second step to fetch github data of user using fetch method
+// async function fetchGitData(username) {
+//     try {
+//         const res = await fetch(`https://api.github.com/users/${username}`)
+//         const user = await res.json()
+//         console.log(user);
+//         displayData(user)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// // this is my third step to display the data of user in html
+// function displayData(userdata) {
+//     const userDetails = document.querySelector('#user-details')
+
+//     if (userdata.id) {
+//         userDetails.innerHTML = `<aside>
+//             <img src="${userdata.avatar_url}" alt="loading...">
+//         </aside>
+//         <article id="aboutme">
+//             <h1>name: <span>${userdata.name}</span></h1>
+//             <p>Bio: <span>${userdata.bio}.</span></p>
+//             <p>Location: <span>${userdata.location}</span></p>
+//             <div class="summary">
+//                 <button>repos: <span>${userdata.public_repos}</span></button>
+//                 <button>followers: <span>${userdata.followers}</span></button>
+//                 <button>following: <span>${userdata.following}</span></button>
+//             </div>
+//         </article>`
+//     }
+//     else{
+//         userDetails.innerHTML = `<h1 style="color:orange; font-size:25px;padding:15px 50px;">Invalid User name!</h1>`
+//     }
+// }
